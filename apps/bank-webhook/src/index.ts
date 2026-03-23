8b131b68a3e38009bbc930c2a8393a73938e2589
tanmay5268 <tanmay.mewati94@gmail.com>
2026-03-23 22:58:35 +0530

    feat(db): add Prisma schema and generated files for user and transaction models
    - Created a new Prisma schema defining User, Merchant, OnRampTransaction, and Balance models.
    - Added enums for AuthType and OnRampStatus.

--- apps/bank-webhook/src/index.ts
+++ apps/bank-webhook/src/index.ts
@@ -1,9 +1,46 @@
+import { prisma } from "../../../packages/db/index";
 import express from "express";
+import type { Request, Response } from "express";
 const app = express();
-app.post('/webhook',(req, res) => {
-    console.log(req.body);
-    res.status(200).send("OK");
-});
+app.post('/hdfcwebhook', async (req: Request, res: Response) => {
+    const info = {
+        token: req.body.token,
+        userId: req.body.user_identifier,
+        amount: req.body.amount,
+    }
+    try {
+        await prisma.$transaction([
+            prisma.balance.update({
+                where: {
+                    userId: Number(info.userId)
+                },
+                data: {
+                    amount: {
+                        increment: info.amount
+                    }
+                }
+            }),
+            prisma.onRampTransaction.updateMany({
+                where: {
+                    token: info.token
+                },
+                data: {
+                    status: "Success",
+                }
+            })
+        ]);
+        res.status(200).json({
+            message: "transaction successful"
+        });
+    } catch (error) {
+        res.status(500).json({
+            error: error,
+            message: "transaction failed"
+        });
+    };
+}
+
+);
 
 app.listen(5000, () => {
     console.log(`Server is running on ${'http://localhost:5000'}`);
